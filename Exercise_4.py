1625cc96ee50fdac6186cd3ece7d410a440028da
apparajuakhil <94913578+apparajuakhil@users.noreply.github.com>
2024-05-11 15:54:33 -0700

    Completed Exercise_4

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,61 @@
+"""  
+ 
+ Time Complexity : O(nlogn)
+ Space Complexity : O(n)
+ Did this code successfully run on Leetcode : Yes
+ Any problem you faced while coding this : No
+
+
+ Your code here along with comments explaining your approach :
+ Merge Sort algorithm splits the existing array into half and repeats the same again of splitted left and right halves.
+ Once the length reaches <= 1 it returns the element and now it starts to merge back in the sorted order by running a loop against
+ these two halves and we check which is smaller and we store that element in the arr along with incrementing the index based on the
+ array ele we pushed that is if it's left then we increment left_index, if it's right we increment right index. The main array index
+ will be left_index+right_index. Once the loop is done we again check the left_index against length of left array and 
+ right index against length of right array to see if any array still has elements, if yes, we store them back to arr based on left_index+right_index.
+ We repeat this process on the splitted arrays and hence the array is sorted.
+ """
+
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
-  
   #write your code here
   
+  if len(arr) <= 1:
+     return
+  
+  mid = len(arr) // 2
+
+  left_arr = arr[:mid]
+  right_arr = arr[mid:]
+
+  mergeSort(left_arr)
+  mergeSort(right_arr)
+  mergeSortHelper(arr, left_arr, right_arr)
+
+def mergeSortHelper(arr, left_arr, right_arr):
+  left_index, right_index = 0, 0
+  
+  while left_index < len(left_arr) and right_index < len(right_arr):
+    if left_arr[left_index] <= right_arr[right_index]:
+      arr[left_index + right_index] = left_arr[left_index]
+      left_index += 1
+    else:
+      arr[left_index + right_index] = right_arr[right_index]
+      right_index += 1
+
+  while left_index < len(left_arr):
+    arr[left_index + right_index] = left_arr[left_index]
+    left_index += 1
+
+  while right_index < len(right_arr):
+    arr[left_index + right_index] = right_arr[right_index]
+    right_index += 1
+
 # Code to print the list 
 def printList(arr): 
-    
     #write your code here
+    for ele in arr:
+        print(f"{ele} \n")
   
 # driver code to test the above code 
 if __name__ == '__main__': 
--- Exercise_5.py
+++ Exercise_5.py
@@ -3,8 +3,38 @@
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
   #write your code here
+  pivot = arr[h]
+  i = l - 1
+
+  for j in range(l,h):
+    if arr[j] <= pivot:
+      i += 1
+      arr[i], arr[j] = arr[j], arr[i]
+  
+  arr[i+1], arr[pivot] = arr[pivot], arr[i+1]
 
 
 def quickSortIterative(arr, l, h):
   #write your code here
+  
+  stack = [(arr, l, h)]
+
+  while len(stack) < 0:
+    arr, l, h = stack.pop()
+    print(arr)
+    if l < h:
+      pivot = partition(arr, l, h)
+
+      stack.push(arr, l, pivot - 1)
+      stack.push(arr, pivot + 1, h)
+
+  
+# Driver code to test above 
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSortIterative(arr,0,n-1) 
+print ("Sorted array is:") 
+for i in range(n): 
+    print ("%d" %arr[i]), 
+
 
